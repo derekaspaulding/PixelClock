65c6f9ef176755bd7ef239cd69627bd2c1c7d22d
Derek Spaulding <spaul048@umn.edu>
2017-01-01 03:03:43 -0600

    Clock Functionality implemented
    added clock functionality which can display time with or without
    seconds.

--- clock.js
+++ clock.js
@@ -2,9 +2,76 @@
 window.clock = window.clock || {};
 
 function Clock() {
-	this.init = function() {
+	this.startClock = function() {
+		window.clock.controller.clearInterval();
+		window.clock.controller.currentInterval = setInterval(this.drawClock.bind(this), 1000);
+	}
+
+	this.drawClock = function(seconds) {
+		window.clock.controller.clearAllPixels();
+		// var now = new Date(2016, 12, 31, 11, 13, 43, 500);
+		var now = new Date();
+		var hour = now.getHours() % 12;
+		var hour2;
+		var min = now.getMinutes();
+		var sec = seconds ? now.getSeconds() : -1;
+
+		if(hour >= 10) {
+			hour2 = hour % 10;
+			hour = Math.floor(hour / 10);
+		}
+
+		var min1 = Math.floor(min / 10);
+		var min2 = min % 10;
+
+		var mask = window.clock.chars.getMask(hour.toString());
+
+		if(hour2 >= 0){
+			mask = this.addCharToMask(mask, hour2.toString());
+		}
+
+		mask = this.addCharToMask(mask, ":");
+
+		mask = this.addCharToMask(mask, min1.toString());
+		mask = this.addCharToMask(mask, min2.toString());
+
+		if(sec >= 0) {
+			var sec1 = Math.floor(sec / 10);
+			var sec2 = sec % 10;
+			
+			mask = this.addCharToMask(mask, ":");
+			mask = this.addCharToMask(mask, sec1.toString());
+			mask = this.addCharToMask(mask, sec2.toString());
+		}
+		
+		window.clock.controller.stampCenter(mask);
+	}
+
+	this.addCharToMask = function(mask, ch) {
+		if(!Array.isArray(mask)) {
+			return false;
+		}
+		var charMask = window.clock.chars.getMask(ch);
+		if(!charMask) {
+			return false;
+		}
+
+		if(charMask.length !== mask.length) {
+			return false;
+		}
+
+		for(var i = 0; i < mask.length; i++) {
+			mask[i].push(0);
+		}
+
+		for(var y = 0; y < charMask.length; y++) {
+			for(var x = 0; x < charMask[y].length; x++) {
+				mask[y].push(charMask[y][x]);
+			}
+		}
+
+		return mask;
 	}
 }
 
 window.clock.clock = new Clock();
-window.clock.clock.init();
--- controller.js
+++ controller.js
@@ -2,6 +2,7 @@
 window.clock = window.clock || {};
 
 function Controller() {
+	this.currentInterval;
 	this.pixelMatrix = [];
 	this.canvas = document.querySelector('#pixelCanvas');
 	this.ctx;
@@ -14,29 +15,67 @@ function Controller() {
 		b: 0
 	}
 
+	this.letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890:";
+	this.lettersIndex = 0;
+	
 	this.init = function () {
 		this.canvas.width = this.canvas.scrollWidth;
 		this.canvas.height = this.canvas.scrollHeight;
 		this.ctx = this.canvas.getContext('2d');
 
 		this.pixelSize = 20;
-		this.width = 37;
-		this.height = 8;
+		this.width = 40;
+		this.height = 24;
 
 		for(var i = 0; i < this.width; i++) {
 			for(var j = 0; j < this.height; j++) {
+				/*
 				var r = Math.floor(i * (127.5 / this.width) + j * (127.5 / this.height));
 				var g = Math.floor(i * (127.5 / this.width) + j * (127.5 / this.height));
 				var b = Math.floor(i * (127.5 / this.width) + j * (127.5 / this.height));
-				this.fillPixel(i, j, r, g, b)
+				*/
+				this.fillPixel(i, j, this.bgColor.r, this.bgColor.g, this.bgColor.b)
 			}
 		}
+		/*
+		this.stamp(0, 1, window.clock.chars.getMask('0'));
+		this.stamp(4, 1, window.clock.chars.getMask('1'));
+		this.stamp(7, 1, window.clock.chars.getMask(':'));
+		this.stamp(10, 1, window.clock.chars.getMask('0'));
+		this.stamp(14, 1, window.clock.chars.getMask('9'));
+		*/
+
+		var next = document.querySelector('#next');
+		var clockStart = document.querySelector('#clock');
+
+		next.addEventListener('click', function(e) {
+			var mask = window.clock.chars.getMask(window.clock.controller.letters[window.clock.controller.lettersIndex]);
+			window.clock.controller.lettersIndex = window.clock.controller.lettersIndex == window.clock.controller.letters.length - 1 ? 0 : window.clock.controller.lettersIndex + 1;
+
+			window.clock.controller.clearAllPixels();
+			window.clock.controller.stampCenter(mask);
+		});
+
+		clockStart.addEventListener('click', function(e) {
+			clearInterval(this.currentInterval);
+			window.clock.clock.startClock();
+		});
 	};
+
+	this.clearInterval = function() {
+		if(Array.isArray(this.currentInterval)) {
+			for(var i = 0; i < this.currentInterval.length; i++) {
+				clearInterval(this.currentInterval[i]);
+			}
+		}
+		else if((typeof this.currentInterval) === 'number') {
+			clearInterval(this.currentInterval);
+		}
+	}
 
 	this.fillPixel = function(x, y, r, g, b) {
 		var color = "rgb(" + r + ", " + g + ", " + b + ")";
 		var bgColor = "rgb(" + this.bgColor.r + ", " + this.bgColor.g + ", " + this.bgColor.b + ")";
-		console.log(color);
 
 		this.ctx.beginPath();
 		this.ctx.rect(x * this.pixelSize, y * this.pixelSize, this.pixelSize, this.pixelSize);
@@ -50,15 +89,31 @@ function Controller() {
 		this.fillPixel(x, y, this.bgColor.r, this.bgColor.g, this.bgColor.b);
 	};
 
+	this.clearAllPixels = function () {
+		for(var i = 0; i < this.width; i++){
+			for(var j = 0; j < this.height; j++) {
+				this.clearPixel(i, j);
+			}
+		}
+	};
+
 	this.stamp = function(x, y, mask) {
 		for(var i = 0; i < mask.length; i++) {
 			for(var j = 0; j < mask[i].length; j++) {
 				if(mask[i][j].r && mask[i][j].g && mask[i][j].b) {
-					this.fillPixel(x + i, y + j, mask[i][j].r, mask[i][j].g, mask[i][j].b);
+					this.fillPixel(x + j, y + i, mask[i][j].r, mask[i][j].g, mask[i][j].b);
 				}
 			}
 		}
 	};
+
+	this.stampCenter = function(mask) {
+		var height = mask.length;
+		var width = mask[0].length;
+		var marginX = Math.floor((this.width - width) / 2);
+		var marginY = Math.floor((this.height - height) / 2);
+		this.stamp(marginX, marginY, mask);
+	}
 
 	this.getRandomColor = function() {
 		var values = '123456789ABCDEF';
